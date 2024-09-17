5778555f084e2cad8c76e2e59581c949ac4b13d3
Vivian Mak <vmak@olin.edu>
2024-09-16 23:15:55 -0400

    finsihed code for wall following
    - add pid and better adjustments as next steps

--- warmup_project/warmup_project/wall_follower.py
+++ warmup_project/warmup_project/wall_follower.py
@@ -3,67 +3,103 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Header
+import math
 
-# from neato2_interfaces.msg import Bump
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-from neato2_interfaces.msg import Scan 
+from rclpy.parameter import Parameter
+from rcl_interfaces.msg import SetParametersResult
+from rclpy.qos import qos_profile_sensor_data
 
 
 class wallFollowing(Node):
-    """Thisis a msg sending node, which inherits from Node. Sends that the robot detects a wall"""
+    """Sends message that the robot detects a wall"""
 
     def __init__(self):
         """Initializes the class"""
 
         # Create a node
-        super().__init__("send_message_node")  # calls the node "send_message_node"
+        super().__init__("wall_follower")
 
         # Initialize variables
-        self.detect_wall_state = False
+        self.align_state = False
         self.turn_angle = 0
+        self.slope = 0
 
-        # Create a timer
-        timer_period = 0.1
-        self.timer = self.create_timer(timer_period, self.run_loop)
+        # Create a timer -- runs until the laser data sense that it is close to a wall
+        self.timer = self.create_timer(0.1, self.run_loop)
 
         # Create a publisher for the motors
-        self.publisher = self.create_publisher(, "", 10)
-
+        self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
 
         # Create subscriber to the bumper sensor
-        self.sub = self.create_subscription(, "", self., 10)
+        self.sub = self.create_subscription(LaserScan, "scan", self.process_scan, 10)
 
+    def process_scan(self, msg):
+        """
+        Callback for handling a wall detector sensor input.
 
-    def process_bump(self, msg):
-        """Callback for handling a wall detector sensor input."""
+        Args:
+
+        """
+        # Initialize variables
+        theta = math.radians(45)
+        index1 = 270
+        index2 = 271
+
+        # Find two points from list of ranges and convert them to x and y coordinates
+        x1 = msg.ranges[index1] * math.cos(theta)
+        y1 = msg.ranges[index1] * math.sin(theta)
+        x2 = msg.ranges[index2] * math.cos(theta + msg.angle_increment)
+        y2 = msg.ranges[index2] * math.sin(theta + msg.angle_increment)
+
+        # debug
+        print("The ranges on left side is:")
+        print(msg.ranges[90])
+
+        # print(f"the angle min is: {msg.angle_min}")
+        # print(f"the angle max is: {msg.angle_max}")
+        # print(f"the angle increment is: {msg.angle_increment}")
+
+        # Find the slope of the wall
+        self.slope = (y2 - y1) / (x2 - x1)
+
+        # Find the angular error for the Neato to adjust
+        self.turn_angle = math.radians(180 - math.atan(self.slope))
+
+        # print(f"the slope is {self.slope}")
+
         # Process msg
-        self.detect_wall_state = (
-            # if the lidar bounces
-        )
-        # Determine if a bump has happened
-        # Set the bumper state variable
-
-        # Logic to determine how much to turn
-        self.turn_angle = msg.angle_increment # angle_min or angle_max
+        if self.turn_angle != 0:
+            self.align_state = True
 
     def run_loop(self):
-        """Move robot and turn wheel towards wall"""
+        """Move robot towards detected wall"""
         # Set the subscriber to be Twist type
-        cmd_vel = Twist
+        cmd_vel = Twist()
 
-        # Command the robot to move
-        # If there is a wall, turn towards wall and go towards it
-        cmd_vel.angular.z = self.turn_angle
-        cmd_vel.linear.x = 0.1
+        # Initially set the forware velocity and angular state to 0
+        if self.align_state is False:
+            cmd_vel.linear.x = 0.0
+            cmd_vel.angular.z = 0.0
+        else:
+            # cmd_vel.angular.z = -0.1
+            if self.slope < 0:
+                # Turn counterclockwise
+                cmd_vel.angular.z = 0.1
+            else:
+                # Turn clockwise
+                cmd_vel.angular.z = -0.1  # self.turn_angle / self.timer
+                cmd_vel.linear.x = 0.1
+
+        # cmd_vel.linear.x = 0.1
 
         self.publisher.publish(cmd_vel)
 
+
 def main(args=None):
-    """Initializes a node, runs it, and cleans up after termination.
-    Input: args(list) -- list of arguments to pass into rclpy. Default None.
-    """
+    """ """
     rclpy.init(args=args)  # Initialize communication with ROS
     node = wallFollowing()  # Create our Node
     rclpy.spin(node)  # Run the Node until ready to shutdown
