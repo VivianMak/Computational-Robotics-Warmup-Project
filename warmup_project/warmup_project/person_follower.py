8e66f22c95c79b46ef21ddfdd081c91aaff831c0
eddydpan <eddydotpan@gmail.com>
2024-09-23 14:41:47 -0400

    Finish docstrings and comments in person_follower

--- warmup_project/warmup_project/person_follower.py
+++ warmup_project/warmup_project/person_follower.py
@@ -1,34 +1,9 @@
+"""A node that controls the vehicle to identify and autonomously follow a person around."""
+
 import rclpy  
-from tf_transformations import euler_from_quaternion
 from rclpy.node import Node 
-from neato2_interfaces.msg import Bump 
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
-import math, time
-
-def PI_control(dir, error, e_integral):
-
-    if dir == "linear":
-        dt = 0.6    
-        Kp = 1.0
-        Ki = 0.001
-        e_integral = e_integral * dt
-
-        speed = dt * (Kp * error) + Ki * e_integral
-        if speed > 0.25:
-            speed = 0.25
-    elif dir == "angular":
-        dt = 0.4    
-        Kp = 1.0
-        Ki = 0.001
-        e_integral = e_integral * dt
-        speed = dt * (Kp * error) + Ki * e_integral
-        if speed > 0.35:
-            speed = 0.35
-    
-    return speed
-
 
 class PersonFollowerNode(Node):
     """
@@ -41,24 +16,23 @@ class PersonFollowerNode(Node):
     Publishers Needed:
         - Twist cmd_vel message, which commands vehicle velocity.
     Subscribers Needed:
-        - Odometry pose message, which reads the position of the robot.
+        - LaserScan scan message, which reads the distance and angle of objects
+          around the vehicle.
     """
     def __init__(self):
         """Initializes the class."""
         super().__init__("person_follower_node") # node names should be unique
         # Create a timer that runs the robot motors
         self.create_timer(0.1, self.run_loop)
+        # Initialize how wide the person-following zone is for the Neato
         self.degree_of_view = 15
-        self.max_range = 1.5 # The max distance we limit the Neato's IR to see
-        self.r_list = None
+        # Initialize the max distance we limit for the person-follower range. Reduces noise.
+        self.max_range = 1.5 
+        # Initialize the distance to stop before reaching the person. Includes buffer for
+        # imprecise person location. 
         self.distance_from_person = 0.6
-
-        self.dt = 0.3
-        self.kP = 1.0
-        self.e_integral = 0
-
-
-
+        # Initialize variable for the list of distances in the cone of view
+        self.r_list = None
         # Initialize the publisher for cmd_vel
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         # Initialize subscriber for LiDAR scan
@@ -66,20 +40,19 @@ class PersonFollowerNode(Node):
 
     def process_scan(self, msg):
         """
-        Callback for handling a wall detector sensor input.
+        Callback for handling the LiDAR scan points in the degree of view.
 
-        Args:
-
+        Args: msg (Laserscan): a Laserscan type message from the subscriber
         """
-        r_list = []
-        # Initialize variables
+        r_list = [] 
+        # Loop through each index scans, each index representing a degree from 0-360
         for idx in range(-self.degree_of_view, self.degree_of_view):
             dist = msg.ranges[idx]
             r_list.append(dist)
-        self.r_list = r_list
+        self.r_list = r_list 
 
     def run_loop(self):
-        """Tracks the relative position of the person and tracks them."""
+        """Tracks the relative position of the person and follows them."""
 
         # Create a Twist message to describe the robot motion
         vel = Twist()
@@ -92,7 +65,7 @@ class PersonFollowerNode(Node):
             for idx, r in enumerate(self.r_list):
                 # Remove all LiDAR points that don't exist (inf) or are too far away. Reduces noise.
                 if r != float('inf') and r <= self.max_range:
-                    person_points[idx] = r # adds index of person_points in case there is a gap the LiDAR scan (i.e 11, 12, 14, 15)
+                    person_points[idx] = r # includes index of person_points in case there is a gap the LiDAR scan (i.e 11, 12, 14, 15)
                     sum_person_r += r
                     sum_person_angle_idx += idx
 
@@ -102,36 +75,25 @@ class PersonFollowerNode(Node):
             else:
                 # Estimate that the person is at the average of the distances and angles taken from the LiDAR scan
                 average_r = sum_person_r / len(person_points)
-                average_person_angle_idx = sum_person_angle_idx / len(person_points) - self.degree_of_view # to correct for idx starting at 0 instead of -15
+                # Calculate angle that the person is located from the vehicle's heading. Subtract to correct 
+                # for idx starting at 0 instead of -self.degree_of_view
+                average_person_angle_idx = sum_person_angle_idx / len(person_points) - self.degree_of_view 
+                # The person is the endpoint, represented in polar coordinates (r,theta)
                 polar_endpoint = (average_r, average_person_angle_idx)
                 print(f"Polar endpoint: {polar_endpoint}")
-                
-                # print(f"delta angle {rel_pos[2] - polar_endpoint[1]}") # come back to this, why is delta angle 13.8 while polar_endpoint[1] is -13.5?
 
-                
-
-                if polar_endpoint[1] > 0.05:
-                    
-                    # vel.angular.z = PI_control("angular", error, self.e_integral)
-                    # vel.linear.x = PI_control("linear", error, self.e_integral)
-
-                    vel.angular.z = 0.1
-
-                elif polar_endpoint[1] < -0.05:
-                    vel.angular.z = -0.1
-
+                # Rotation to align heading with person
+                if polar_endpoint[1] > 0.05: # Person is on the left of the Neato
+                    vel.angular.z = 0.15
+                elif polar_endpoint[1] < -0.05: # Person is on the right of the Neato
+                    vel.angular.z = -0.15
                 else:
-                    vel.angular.z = 0.0
-
-                if polar_endpoint[0] > self.distance_from_person:
-                    # speed = self.distance_from_person / self.kP
-                    # if speed > 0.25: speed = 0.25
-                    # vel.linear.x = speed
-                    vel.linear.x = 0.15
+                    vel.angular.z = 0.0 # Set the angular velocity to 0 if the person is approx. straight ahead
+                # Linear movement to follow person 
+                if polar_endpoint[0] > self.distance_from_person: # Ensure the vehicle doesn't crash into the person. Maintain a distance.
+                    vel.linear.x = 0.1
                 else:
                     vel.linear.x = 0.0
-
-
         # Publish the Twist message to cmd_vel target
         self.publisher.publish(vel)
 
